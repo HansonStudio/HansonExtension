ee1b22da08bf8d9a12fb3da1f71ed45f44c8c25a
Hanson <hansenhs21@live.com>
2017-11-09 01:16:28 +0800

    add UIApplication Extension

--- HansonExtension-Demo/ViewController.swift
+++ HansonExtension-Demo/ViewController.swift
@@ -7,17 +7,20 @@
 //
 
 import UIKit
+import HansonExtensionKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        _ = UIApplication.hs.presentedViewController()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
     }
 
 
---
+++ HansonExtensionKit/Sources/UIApplication+Extension.swift
@@ -0,0 +1,36 @@
+//
+//  UIApplication+Extension.swift
+//  HansonExtensionKit
+//
+//  Created by Hanson on 2017/11/9.
+//  Copyright © 2017年 HansonStudio. All rights reserved.
+//
+
+import Foundation
+import UIKit
+extension UIApplication: HansonCompatible { }
+extension HansonExtensionKit where T == UIApplication {
+    
+    /// 获取当前正在展示的`ViewController`
+    ///
+    /// - Parameter rootController: 指定root controller
+    /// - Returns: 正在展示的`ViewController`
+    public static func presentedViewController(rootController: UIViewController? = UIApplication.shared.keyWindow?.rootViewController) -> UIViewController? {
+        
+        if let navigationController = rootController as? UINavigationController {
+            return presentedViewController(rootController: navigationController.visibleViewController)
+        }
+        
+        if let tabBarController = rootController as? UITabBarController {
+            if let selectedController = tabBarController.selectedViewController {
+                return presentedViewController(rootController: selectedController)
+            }
+        }
+        
+        if let presented = rootController?.presentedViewController {
+            return presentedViewController(rootController: presented)
+        }
+        
+        return rootController
+    }
+}
