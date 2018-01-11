35ff7a09b1103a0125e87b2fa46dc305c154498b
zyphs21 <hansenhs21@live.com>
2018-01-11 15:45:51 +0800

    add validate function in String Extension

--- HansonExtensionKit/Sources/String+Extension.swift
+++ HansonExtensionKit/Sources/String+Extension.swift
@@ -11,6 +11,7 @@ import CommonCrypto
 
 extension String: HansonExtensionProtocol { }
 extension HansonExtensionKit where T == String {
+    
     public func MD5String() -> String {
         let cStr = wrappedValue.cString(using: String.Encoding.utf8);
         let buffer = UnsafeMutablePointer<UInt8>.allocate(capacity: 16)
@@ -37,5 +38,62 @@ extension HansonExtensionKit where T == String {
         }
         result.deinitialize()
         return String(format: hash as String)
+    }
+    
+    
+    // MARK: -
+    
+    public func validate(_ RegEx: String) -> Bool {
+        let predicate = NSPredicate(format: "SELF MATCHES %@", RegEx)
+        return predicate.evaluate(with: wrappedValue)
+    }
+    
+    public func validateNickname() -> Bool {
+        return self.validate("^.{3,30}$")
+    }
+    
+    public func validateAllNum() -> Bool {
+        return self.validate("^\\d*$")
+    }
+    
+    public func validatePhoneNum() -> Bool {
+        return self.validate("^\\d{11}$")
+    }
+    
+    public func validatePassword() -> Bool {
+        return self.validate("^[A-Z0-9a-z]{6,20}")
+    }
+    
+    public func validateEmail() -> Bool {
+        return self.validate("[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}")
+    }
+    
+    // MARK: -
+    //48-57num  65-90A 97-122a
+    
+    public func allNumber() -> Bool {
+        if let asciiString = wrappedValue.cString(using: String.Encoding.ascii) {
+            for v in 0 ..< asciiString.count - 1 {
+                if !(asciiString[v] >= 48 && asciiString[v] <= 57) {
+                    return false
+                }
+            }
+            return true
+        } else {
+            return true
+        }
+    }
+    
+    public func allLetter() -> Bool {
+        if let asciiString = wrappedValue.cString(using: String.Encoding.ascii) {
+            for v in 0 ..< asciiString.count - 1 {
+                if !((asciiString[v] >= 65 && asciiString[v] <= 90) || (asciiString[v] >= 97 && asciiString[v] <= 122)) {
+                    return false
+                }
+            }
+            return true
+        } else {
+            return true
+        }
     }
 }
